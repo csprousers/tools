ad405efaebad0f6161499b038b07133b2b8d123a
Oryxamus <greg@csprousers.org>
2015-04-22 17:10:57 -0400

    CSPro.NET: added a class that contains useful dictionary-related routines

---
+++ CSPro.NET/DataDictionaryWorker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+// some useful routines
+
+namespace CSPro
+{
+    public class DataDictionaryWorker
+    {
+        public static void CalculateRecordLengths(DataDictionary dict)
+        {
+            foreach( Level level in dict.Levels )
+            {
+                int maxLengthIDs = CalculateMaxLengthFromItems(level.IDs.Items);
+
+                foreach( Record record in level.Records )
+                    record.Length = Math.Max(maxLengthIDs,CalculateMaxLengthFromItems(record.Items));
+            }
+        }
+
+        private static int CalculateMaxLengthFromItems(List<Item> items)
+        {
+            int maxLength = 0;
+
+            foreach( Item item in items )
+                maxLength = Math.Max(maxLength,item.Start + item.Length - 1);
+
+            return maxLength;
+        }
+
+    }
+}
